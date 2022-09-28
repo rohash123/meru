33195eee1047fd6c2b3f18ec750ec5ca73119f98
Rohan  Arora <rohanarora@Rohans-MacBook-Air.local>
2022-09-28 11:14:26 -0700

    googleauthwvars

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ Amplify.configure(awsExports);
 function App({ signOut, user }) {
   return (
     <>
-      <h1>Hello {user.username}</h1>
+      <h1>Hi {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
     </>
   );
